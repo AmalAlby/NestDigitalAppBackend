4a29082f5267131d1e481e17eb27ee9498b3bd74
AmalAlby <amalalbyn@gmail.com>
2022-12-12 11:24:54 +0530

    app created

---
+++ src/main/java/com/nest/nestdigitalapplicationbackend/NestdigitalapplicationbackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.nestdigitalapplicationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestdigitalapplicationbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestdigitalapplicationbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nest/nestdigitalapplicationbackend/NestdigitalapplicationbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.nestdigitalapplicationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestdigitalapplicationbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
